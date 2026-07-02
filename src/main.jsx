16b10e16a7c889e434e598bbfe8a2712009f683d
nabeel151 <nabeel.shahbaz22@gmail.com>
2026-07-02 17:05:58 +0500

    add more code all

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { ProductProvider } from "./context/ProductContext.jsx";
+import { ProductProvider } from "./context/ProductContext";
 import { CategoryProvider } from "./context/CategoryContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
